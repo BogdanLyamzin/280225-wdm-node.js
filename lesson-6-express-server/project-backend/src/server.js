899b0104814d36b236af60b1804cb959f5f89d11
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-10-01 15:18:50 +0200

    finish lesson 7

--- lesson-6-express-server/project-backend/src/server.js
+++ lesson-6-express-server/project-backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+
 
 import productsRouter from "./routers/products.router.js";
 
---
+++ lesson-7-express-server-sequelize-cloud-postgreSQL/project-backend/src/routers/users.router.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { getUsersController, addUserController } from "../controllers/users.controller.js";
+
+const usersRouter = Router();
+
+usersRouter.get("/", getUsersController);
+
+usersRouter.post("/", addUserController);
+
+export default usersRouter;
