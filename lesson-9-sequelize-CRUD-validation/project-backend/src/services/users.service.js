351b99fa1d3fcfeecfbd2483ee25dd0c32868209
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-10-06 11:32:19 +0200

    finish lesson 9

---
+++ lesson-9-sequelize-CRUD-validation/project-backend/src/services/users.service.js
@@ -0,0 +1,21 @@
+import { QueryTypes } from "sequelize";
+
+import sequelize from "../db/sequelize.js";
+
+export const getUsers = async () => {
+  const query = `SELECT * FROM users`;
+  const data = await sequelize.query(query, {
+    type: QueryTypes.SELECT,
+  });
+
+  return data;
+};
+
+export const addUser = async ({ name, email }) => {
+  const query = `INSERT INTO users (name, email) VALUES('${name}', '${email}') RETURNING *`;
+  const data = await sequelize.query(query, {
+    type: QueryTypes.INSERT,
+  });
+
+  return data[0][0];
+};
