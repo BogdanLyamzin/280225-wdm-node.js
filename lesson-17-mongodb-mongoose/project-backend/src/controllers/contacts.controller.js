dcb71c5089b60c7cd3a643654682a457049d77f1
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-10-20 13:00:33 +0200

    finish lesson 17

---
+++ lesson-17-mongodb-mongoose/project-backend/src/controllers/contacts.controller.js
@@ -0,0 +1,51 @@
+import HttpError from "../utils/HttpError.js";
+
+import {
+  getContacts,
+  getContactById,
+  addContact,
+  updateContactById,
+  deleteContactById,
+} from "../services/contacts.service.js";
+
+import { contactFullSchema } from "../schemas/contact.schemas.js";
+
+import validateBody from "../utils/validateBody.js";
+
+export const getContactsController = async (req, res) => {
+  const {id: owner} = req.user;
+  const result = await getContacts({owner});
+  res.json(result);
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { id } = req.params;
+  const result = await getContactById(id);
+  if (!result) throw HttpError(404, `Contact with id=${id} not found`);
+
+  res.json(result);
+};
+
+export const addContactController = async (req, res) => {
+  validateBody(contactFullSchema, req.body); 
+  const {id: owner} = req.user;
+  const result = await addContact({...req.body, owner}); 
+  res.status(201).json(result);
+};
+
+export const updateContactByIdController = async (req, res) => {
+  const { id } = req.params;
+  const result = await updateContactById(id, req.body);
+  if (!result) throw HttpError(404, `Contact with id=${id} not found`);
+
+  res.json(result);
+};
+
+export const deleteContactByIdController = async (req, res) => {
+  const { id } = req.params;
+  const result = await deleteContactById(id);
+  if (!result) throw HttpError(404, `Contact with id=${id} not found`);
+
+  // res.status(204).send();
+  res.json(result);
+};
